2d7de1e3e24d5d7af7c6502cd0e2a301e5bf245e
Raprap <rafaeldnapoles@gmail.com>
2026-03-26 23:57:36 +0800

    removed the sidebar and navbar from login and signup page (user)

--- resources/js/Layouts/UserLayout.vue
+++ resources/js/Layouts/UserLayout.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="user-layout">
-    <UserSidebar />
-    <div class="main-wrapper">
-      <UserNavbar />
+    <UserSidebar v-if="showLayoutParts" />
+    <div :class="showLayoutParts ? 'main-wrapper' : 'full-wrapper'">
+      <UserNavbar v-if="showLayoutParts" />
       <main class="page-content">
         <router-view />
       </main>
@@ -11,8 +11,16 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 import UserSidebar from '../components/user/sidebar.vue'
 import UserNavbar from '../components/user/navbar.vue'
+
+const route = useRoute()
+
+const showLayoutParts = computed(() => {
+  return route.path !== '/user' && route.path !== '/user/signup'
+})
 </script>
 
 <style scoped>
@@ -25,6 +33,13 @@ import UserNavbar from '../components/user/navbar.vue'
 .main-wrapper {
   margin-left: 317px;
   width: calc(100% - 317px);
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.full-wrapper {
+  width: 100%;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
